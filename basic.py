cfd636c977e50816da59532b768aeb5d6f1dad2d
afzalhossainraju100 <raju23105341031@diu.edu.bd>
2025-10-26 18:59:38 +0600

    Larn about python input

--- basic.py
+++ basic.py
@@ -4,15 +4,10 @@ if False:
 else:
     print("This script is being imported as a module.")
     print("Module-level code can go here.") 
-    print("Module-level code can go here.")
-    print("Module-level code can go here.")
-    print("Module-level code can go here.")
-    print("Module-level code can go here.")
-
-
-
-
-    print("Module-level code can go here.")
-    print("Module-level code can go here.")
-    print("Module-level code can go here.")
-
+# input learning
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print(f"Hello, {name}!")
+print(f"You are {age} years old.")
+print(type(name),name)
+print(type(age),age)
