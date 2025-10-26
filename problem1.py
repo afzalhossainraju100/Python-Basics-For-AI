b545ffc3d25832aabc30ef4c353ff5832e5dd4ad
afzalhossainraju100 <raju23105341031@diu.edu.bd>
2025-10-26 19:08:05 +0600

    Little python problem 2 solve

--- problem1.py
+++ problem1.py
@@ -3,3 +3,9 @@ num2 = int(input("Enter second number: "))
 
 sum = num1+num2
 print(f"the sum of {num1} and {num2} is {sum}")
+
+side1 = float(input("Enter length of side 1: "))
+side2 = float(input("Enter length of side 2: "))
+
+area = side1 * side2
+print(f"The area of the rectangle with sides {side1} and {side2} is {area}")
